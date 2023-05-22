c7107337bc9279e6ddc6e49fc13750446429efb2
zainSolcoders <zain@solscoders.com>
2023-05-22 20:15:42 +0500

    post comment

--- firstapp/resources/views/shopify/ajax/posts.blade.php
+++ firstapp/resources/views/shopify/ajax/posts.blade.php
@@ -5,7 +5,21 @@
         
             <span> {{ $post->message }} </span>
             @empty
-            No Record Found
+            No Post Found
             @endforelse
         </div>
+        <hr>
+        <form action="/create_post" method="POST">
+        @csrf
+                        <input type="hidden" name="shop" value="{{ $store->shop_url }}">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label for="product_feature_enable">Post</label><br>
+                                <textarea name="comment" class="form-control" style="height:200px"></textarea> 
+                            </div>
+                        </div>
+                        
+           
+                        <button type="submit" class="btn btn-success">Create Post</button>
+                    </form>
     </div>
